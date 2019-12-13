59f75ee5dfb35711a6491d27736447bdf5f99d18
Joo Sueong Sum <joosueong.sum@viewpoint.com.my>
2019-12-13 12:16:31 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DatabaseWinAuthChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string db = ConfigurationManager.AppSettings["db"];
+            string server = ConfigurationManager.AppSettings["server"];
+            Console.WriteLine($"DB:{db}; Server:{server}; Windows Authentication: {Environment.UserName}");
+            var connString = $"Server= {server}; Database= {db}; Integrated Security=True;";
+            SqlConnection con = new SqlConnection(connString);
+            try
+            {
+                con.Open();
+                con.Close();
+                Console.WriteLine("Connection Success.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Connection Failed.");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
